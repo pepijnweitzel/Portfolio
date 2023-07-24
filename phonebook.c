a8d60c30af430015ebf5c2cba81773dc45f2749d
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-07-24 15:15:48 +0000

    Mon, Jul 24, 2023, 3:15 PM +00:00

--- phonebook.c
+++ phonebook.c
@@ -4,6 +4,8 @@
 int main(void)
 {
     int age = get_int("enter your age: ");
+    string name = get_string("enter your name: ");
 
+    printf("Your name is %s \n", name);
     printf("You are %i years old\n", age);
 }
