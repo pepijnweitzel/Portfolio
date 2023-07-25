a2b8cf1e6a13c3d70962233a2637569f08dee0fe
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-07-25 09:32:13 +0000

    Tue, Jul 25, 2023, 11:32 AM +02:00

--- population.c
+++ population.c
@@ -6,5 +6,7 @@ int main(void)
     int start = get_int("What is the starting population of lama's? \n");
     int goal = get_int("What is the goal population of lama's? \n");
 
+    //every year n/3 get born and n/4 die
+    
     printf("%i %i\n", start, goal);
 }
