6efd671768fe89a9f42c4dc67efb558fe8931215
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-07-27 09:09:31 +0000

    Thu, Jul 27, 2023, 11:09 AM +02:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,11 +1,11 @@
-//Code created by Pepijn Weitzel at 27/7/2023
-#include <stdio.h>
+// Code created by Pepijn Weitzel at 27/7/2023
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    //Prompt for users name:
+    // Prompt for users name:
     string name = get_string("What is your name? \n");
-    //Greet user by their name:
+    // Greet user by their name:
     printf("Hello, %s\n", name);
 }
