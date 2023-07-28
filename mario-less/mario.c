4cbc800a5a8f06adcc72a1fd612701d35718bc4f
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-07-28 11:09:23 +0000

    Fri, Jul 28, 2023, 1:09 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    do
+    {
+        int height = get_int("Height: \n");
+    }
+    while (height >= 1 && height <= 8);
+
+    printf("%i\n", height);
 }
