1ae3c3937794ac33f363e8a9a188787daca33d65
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-22 11:48:35 +0000

    Tue, Aug 22, 2023, 1:48 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,4 +24,10 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int length = strlen(word);
+
+    for (i = 0; i < length; i++)
+    {
+        
+    }
 }
