a2141be8784680015081af8b571b9a2efa9bb039
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-28 10:34:19 +0000

    Mon, Aug 28, 2023, 12:34 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -72,11 +72,11 @@ bool vote(string name)
         if (name == candidates[i].name)
         {
             candidates[i].votes++;
-            return true;
+            return false;
         }
     }
 
-    return false;
+    return true;
 }
 
 // Print the winner (or winners) of the election
