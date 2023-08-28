2090a8e887296971e1f652a105403e01980ed8e6
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-28 11:03:25 +0000

    Mon, Aug 28, 2023, 1:03 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -92,13 +92,13 @@ void print_winner(void)
         }
     }
 
-   for (int i = 0; i < candidate_count; i++)
-   {
-    if (x == candidates[i].votes)
+    for (int i = 0; i < candidate_count; i++)
     {
-        printf("%s\n", candidates[i].name);
+        if (x == candidates[i].votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
     }
-   }
 
     return;
 }
