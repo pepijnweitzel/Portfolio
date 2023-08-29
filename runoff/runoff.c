bc6a46e16721e56d1f105e0b6ff4978e76f7e31a
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-29 12:08:27 +0000

    Tue, Aug 29, 2023, 2:08 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -203,7 +203,7 @@ bool is_tie(int min)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes != min)
+        if (candidates[i].votes != min && candidates[i].eliminated == false)
         {
             x++;
         }
