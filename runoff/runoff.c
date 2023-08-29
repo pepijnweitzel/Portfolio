2ca6b399bb288d60005f6dd49b084273e72662da
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-29 11:04:03 +0000

    Tue, Aug 29, 2023, 1:04 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,13 +128,7 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    for (int i = 0; i < voter_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            
-        }
-    }
+    
     return false;
 }
 
