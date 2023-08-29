2c7937b2a6b6522e8d91e5b121370cf78ad73733
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-08-29 11:11:08 +0000

    Tue, Aug 29, 2023, 1:11 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -132,7 +132,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].votes++;
+            
         }
     }
     return false;
