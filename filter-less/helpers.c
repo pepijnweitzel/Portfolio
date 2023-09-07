5abf6decf1ec334140cc8794dca9726a22d84812
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-07 12:00:22 +0000

    Thu, Sep 7, 2023, 2:00 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int average = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].Red;
-            int new 
+            
         }
     }
     return;
