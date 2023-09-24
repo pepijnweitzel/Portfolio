c96407d3d8ad66fff4b7f55a7d7c90890faf38ee
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-24 13:05:57 +0000

    Sun, Sep 24, 2023, 3:05 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -64,7 +64,8 @@ bool load(const char *dictionary)
 
         // Get the hash value to insert the node and insert the node at the location
         int hash_value = hash(bufferword);
-        
+        n->next = table[hash_value];
+        table[hash_value] = n;
 
 
     }
