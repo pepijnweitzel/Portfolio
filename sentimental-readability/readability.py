85502bf80cb33b45993c2dca2aff537bdff67334
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 20:45:27 +0000

    Tue, Sep 26, 2023, 10:45 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,11 +1,15 @@
 from cs50 import get_string
 
 
-def main():
-    ...
+text = get_string("Text: ")
+sentences = 0
+for char in text:
+    if char == 
 
 
-def get_text():
-    while True:
-        string = get_string("Text: ")
-        if 
+
+
+
+#0.0588 * L - 0.296 * S - 15.8,
+# where L is the average number of letters per 100 words in the text, and
+# S is the average number of sentences per 100 words in the text.
