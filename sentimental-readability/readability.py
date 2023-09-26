7cc5aea2b83ad27876e567fdc63b4e7e5e6986d2
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 20:59:28 +0000

    Tue, Sep 26, 2023, 10:59 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,7 +8,7 @@ number_words = len(words)
 
 sentences = 0
 for char in text:
-    if char == '.' or char == '!' or char == '?':
+    if char == "." or char == "!" or char == "?":
         sentences += 1
 S = sentences / number_words * 100
 
@@ -28,11 +28,6 @@ else:
     print(f"Grade {grade}")
 
 
-
-
-
-
-
-#0.0588 * L - 0.296 * S - 15.8,
+# 0.0588 * L - 0.296 * S - 15.8,
 # where L is the average number of letters per 100 words in the text, and
 # S is the average number of sentences per 100 words in the text.
