1beb4fd3ff10ed0d856246fb4f9302266df3520a
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 20:23:39 +0000

    Tue, Sep 26, 2023, 10:23 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,28 @@
-# TODO
+from cs50 import get_float
+
+def main():
+    change = get_change()
+    quarters = int(change / 0.25)
+    change = change/quarters
+    dimes = int(change / 0.10)
+    change = change/dimes
+    nickels = int(change / 0.05)
+    change = change/nickels
+    pennies = int(change / 0.01)
+
+    total_number = quarters + dimes + nickels + pennies
+    print(f"{total_number}")
+
+
+
+
+def get_change():
+    while True:
+        n = get_float("Change owed: ")
+        if n > 0:
+            return n
+
+
+
+
+main()
