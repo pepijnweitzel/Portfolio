f2ebc921459cb3fdf64451db32a7e9b4c5f245ed
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 20:58:22 +0000

    Tue, Sep 26, 2023, 10:58 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,10 +9,23 @@ number_words = len(words)
 sentences = 0
 for char in text:
     if char == '.' or char == '!' or char == '?':
-        sentences =+ 1
+        sentences += 1
 S = sentences / number_words * 100
+
+letters = 0
 for char in text:
-    if char.isaplha
+    if char.isalpha():
+        letters += 1
+L = letters / number_words * 100
+
+grade = 0.0588 * L - 0.296 * S - 15.8
+if grade >= 16:
+    print(f"Grade 16+")
+elif grade < 1:
+    print(f"Before Grade 1")
+else:
+    grade = round(grade)
+    print(f"Grade {grade}")
 
 
 
