d845aea592812957da857f1a1c02cd931130cae0
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 20:32:22 +0000

    Tue, Sep 26, 2023, 10:32 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,12 +3,16 @@ from cs50 import get_float
 def main():
     change = get_change()
     quarters = int(change / 0.25)
-    change = round(change - quarters * 0.25)
+    print(quarters)
+    change = change - quarters * 0.25
     dimes = int(change / 0.10)
-    change = round(change - dimes * 0.10)
+    print(dimes)
+    change = change - dimes * 0.10
     nickels = int(change / 0.05)
-    change = round(change - nickels * 0.05)
+    print(nickels)
+    change = change - nickels * 0.05
     pennies = int(change / 0.01)
+    print(pennies)
 
     total_number = quarters + dimes + nickels + pennies
     print(f"{total_number}")
