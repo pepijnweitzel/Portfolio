32f93ba9a0c1a68b6c8511eb2499d45b282586c2
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-26 18:44:53 +0000

    Tue, Sep 26, 2023, 8:44 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,25 @@
+def main():
+    height = get_height()
+    spaties = height
+    for i in range(height):
+        spaties -= 1
+        hashes = i + 1
+        print(" " * spaties + "#" * hashes)
 
 
 
 
+def get_height():
+    while True:
+        try:
+            number = int(input("Height: "))
+        except ValueError:
+            print("Not an number")
+        else:
+            if 1 <= number <= 8:
+                return number
 
 
+
+
+main()
