7b985426140f02720b793df87cab4e3d9ecc92a3
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 17:47:11 +0000

    Thu, Sep 28, 2023, 7:47 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -89,3 +89,27 @@ AND month = 7
 AND day = 28
 AND duration < 60;
 
+-- Get names from the people that made the call
+SELECT name FROM people
+WHERE phone_number IN (
+    SELECT caller FROM phone_calls
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND duration < 60;
+);
+
+-- Check for the same names in last made list from withdraw and leaving bakery and previously made list for simmilarities. Which gave me Bruce and Diana
+SELECT name FROM (
+    SELECT name FROM people
+    WHERE phone_number IN (
+        SELECT caller FROM phone_calls
+        WHERE year = 2021
+        AND month = 7
+        AND day = 28
+        AND duration < 60;
+))
+WHERE name IN (
+    SELECT name FROM (SELECT name FROM people WHERE licen_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND minute <= 25 AND activity = 'exit'))
+    WHERE name IN (SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'))));
+
