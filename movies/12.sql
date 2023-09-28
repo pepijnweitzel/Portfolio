fcedcd054ba39a2f4019eb53dd0c2b3ae5bfdbe7
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 15:54:19 +0000

    Thu, Sep 28, 2023, 5:54 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,11 +1,13 @@
-SELECT title FROM movies
-WHERE id IN (
-    SELECT movie_id FROM stars
-    WHERE person_id = (
-        SELECT id FROM people
-        WHERE name = 'Bradley Cooper'
+SELECT title FROM (
+    SELECT title FROM movies
+    WHERE id IN (
+        SELECT movie_id FROM stars
+        WHERE person_id = (
+            SELECT id FROM people
+            WHERE name = 'Bradley Cooper'
+        )
     )
 )
-
-
-
+WHERE title in (
+    SELECT title FROM movies WHERE id IN (SELECT movie_)
+)
