04a5b8e54a910efd5084263658b3e582029acbd6
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 14:40:25 +0000

    Thu, Sep 28, 2023, 4:40 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,3 @@
 SELECT movies.title, ratings.rating FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
-ORDER BY ratings.rating DESC, movies.title
+ORDER BY ratings.rating DESC, movies.title;
