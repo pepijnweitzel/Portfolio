aceae27542187bfc740da62e019c1704ba668e52
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 16:56:37 +0000

    Thu, Sep 28, 2023, 6:56 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,9 +12,12 @@ AND month = 7;
 -- To find out all the kinds of columns there are in this table
 .schema interviews
 
--- To tell me more about what the witnesses said
-
+-- To tell me more about what the witnesses said. It told me te following things:
+--10 min within theft thief in his car to drive away.
+--before 10:15am thief was withdrawing money from ATM on Leggett Street
+--After theft called accomplice for less than minute, taking earliest flight out of fiftyville at july 29th 2021, accomplice bought flight ticket
 SELECT * FROM interviews
 WHERE year = 2021
 AND day = 28
 AND month = 7;
+
