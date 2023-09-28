9c2c6feb2f999e42983096d57b38acc84b092afb
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 16:21:57 +0000

    Thu, Sep 28, 2023, 6:21 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,13 @@
-SELECT movie_id FROM stars
-WHERE person_id = (
-    SELECT id FROM people
-    WHERE name = 'Kevin Bacon' and birth = 1958
-);
+SELECT name FROM people
+JOIN stars ON stars.person_id = people.id
+WHERE id IN (
+    SELECT stars.person_id FROM stars
+    WHERE stars.movie_id IN (
+        SELECT stars.movie_id FROM stars
+        WHERE stars.person_id = (
+            SELECT id FROm people
+            WHERE name = 'Kevin Bacon' AND birth = 1958
+        )
+    )
+)
+AND name != 'Kevin Bacon';
