7721e22e86821d1618ce7b8c4f1e1f090d15e191
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 16:48:25 +0000

    Thu, Sep 28, 2023, 6:48 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- To find out all kinds of columns there are in this table.
+-- To find out all the kinds of columns there are in this table.
 .schema crime_scene_reports
 
 -- To find the crime scene report about the crime. It told me that the crime took place at 10:15am and all 3 witnesses mentioned the bakery, where the theft took place aswell.
@@ -9,3 +9,7 @@ AND year = 2021
 AND day = 28
 AND month = 7;
 
+-- To find out all the kinds of columns there are in this table
+.schema interviews
+
+--
