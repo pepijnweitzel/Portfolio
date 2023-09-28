eebfe67ccec2e89bd2d218606f19d69026c4588c
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 15:00:29 +0000

    Thu, Sep 28, 2023, 5:00 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,9 @@
-SELECT person_id FROM stars
-WHERE movie_id IN (
-    SELECT id FROM movies
-    WHERE year = 2004
-);
+SELECT name FROM people
+WHERE id IN (
+    SELECT person+id FROM stars
+    WHERE movie_id IN (
+        SELECT id FROM movies
+        WHERE year = 2004
+    )
+)
+ORDER BY birth;
