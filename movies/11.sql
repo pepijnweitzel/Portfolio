20945812607483ce4e18910cf5b7faec0118d77d
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-09-28 15:31:06 +0000

    Thu, Sep 28, 2023, 5:31 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,10 +1,13 @@
-SELECT movie_id FROM ratings
-WHERE movie_id IN (
-    SELECT movie_id FROM stars
-    WHERE person_id = (
-        SELECT id FROM people
-        WHERE name = 'Chadwick Boseman'
+SELECT title FROM movies
+WHERE id IN(
+    SELECT movie_id FROM ratings
+    WHERE movie_id IN (
+        SELECT movie_id FROM stars
+        WHERE person_id = (
+            SELECT id FROM people
+            WHERE name = 'Chadwick Boseman'
+        )
     )
-)
-ORDER BY rating DESC
-LIMIT 5;
+    ORDER BY rating DESC
+    LIMIT 5
+);
