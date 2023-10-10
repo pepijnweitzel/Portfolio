1c0cf96618b657813a9c9033161f5b60ca8a635a
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-10 17:55:36 +0000

    Tue, Oct 10, 2023, 7:55 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -225,7 +225,7 @@ def sell():
         elif not request.form.get("shares") or int(request.form.get("shares")) < 1:
             return apology("missing shares", 400)
         # Ensure user has enough shares to sell
-        number_of_shares =
+        number_of_shares = db.execute("SELECT number FROM stocks WHERE useres_id = ? AND stock = ?", session["user_id"], request.form.get("symbol"))
 
         return redirect("/")
     else:
