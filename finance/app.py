47b304404c19883cd7dadf5975f78c303a316ced
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-10 18:26:14 +0000

    Tue, Oct 10, 2023, 8:26 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+import time
 
 from helpers import apology, login_required, lookup, usd
 
