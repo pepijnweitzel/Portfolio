6c355cda8934017eb1c96314359565fc3d694335
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-12 18:12:40 +0000

    Thu, Oct 12, 2023, 8:12 PM +02:00

--- project/app.py
+++ project/app.py
@@ -3,6 +3,7 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 import time
 
 # Import functions from other file
