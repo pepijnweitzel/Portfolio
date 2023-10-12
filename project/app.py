edfc41538b7a018e60272aeb6239a10705cc4272
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-12 12:45:45 +0000

    Thu, Oct 12, 2023, 2:45 PM +02:00

--- project/app.py
+++ project/app.py
@@ -24,6 +24,10 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+@app.route("/")
+def index():
+    return render_template("login.html")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
