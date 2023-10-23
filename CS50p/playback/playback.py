dc5bcf123922ac215e47b79869ff9f48eee3adba
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-23 19:22:01 +0000

    Mon, Oct 23, 2023, 9:22 PM +02:00

---
+++ CS50p/playback/playback.py
@@ -0,0 +1,8 @@
+# Code created by Pepijn Weitzel
+def main():
+    # Prompt user for input and replace " " with "..."
+    text = input().replace(" ", "...")
+    # Print new text
+    print(text)
+
+main()
