d444482dd92d455cbe16202b197204c8fd35e173
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-23 19:41:18 +0000

    Mon, Oct 23, 2023, 9:41 PM +02:00

--- CS50p/tip/tip.py
+++ CS50p/tip/tip.py
@@ -1,7 +1,18 @@
 # Code created by Pepijn Weitzel
 def main():
-    test = float(input())
-    print(test)
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # TODO
+    d = d.(replace)
+
+
+def percent_to_float(p):
+    # TODO
 
 
 main()
