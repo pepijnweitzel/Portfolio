38f8ad59427628bea2507fe73146ce71a08795e9
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-23 19:19:24 +0000

    Mon, Oct 23, 2023, 9:19 PM +02:00

---
+++ CS50p/indoor/indoor.py
@@ -0,0 +1,8 @@
+# Code created by Pepijn Weitzel
+def main():
+    # Get an input from user and make it lowercase
+    text = input().lower()
+    # Print new lowercased string
+    print(text)
+
+main()
