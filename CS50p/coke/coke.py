85cdb25293d33a9171e70cb21de28450943e8d5f
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 12:13:28 +0000

    Tue, Oct 24, 2023, 2:13 PM +02:00

---
+++ CS50p/coke/coke.py
@@ -0,0 +1,15 @@
+# Code created by Pepijn Weitzel
+
+amount_due = 50
+print(f"Amount Due: {amount_due}")
+
+while amount_due > 0:
+    insertion = int(input("Insert coin: "))
+    if insertion == 5 or 10 or 25
+        amount_due -= insertion
+
+if amount_due == 0:
+    print("Change owed: 0")
+else:
+    print(f"Change owed: {amount_due * -1}")
+    
