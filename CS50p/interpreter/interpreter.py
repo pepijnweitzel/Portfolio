798ffcda69720c04a33b38fe09ba2650b4689b24
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 11:01:14 +0000

    Tue, Oct 24, 2023, 1:01 PM +02:00

--- CS50p/interpreter/interpreter.py
+++ CS50p/interpreter/interpreter.py
@@ -4,9 +4,9 @@ expression = input("Expression: ")
 
 expression = expression.split()
 
-x = expression[0]
+x = int(expression[0])
 y = expression[1]
-z = expression[2]
+z = int(expression[2])
 
 if y == "+":
     print(float(x + z))
