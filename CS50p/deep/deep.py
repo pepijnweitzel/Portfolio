a756e4d1f182e3412eacfc0e991360c0c5f6aeba
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 10:22:12 +0000

    Tue, Oct 24, 2023, 12:22 PM +02:00

--- CS50p/deep/deep.py
+++ CS50p/deep/deep.py
@@ -1,3 +1,10 @@
 # Code created by Pepijn Weitzel
+# Prompt user for answer
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-answer = input("")
+# Check whether answer is correct
+match answer:
+    case "42" | "forty-two" | "Forty Two":
+        print("Yes")
+    case _:
+        print("No")
