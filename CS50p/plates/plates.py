504703b593a11e265b1fd72ddfda7076dba34072
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 12:31:56 +0000

    Tue, Oct 24, 2023, 2:31 PM +02:00

--- CS50p/plates/plates.py
+++ CS50p/plates/plates.py
@@ -1,1 +1,17 @@
 # Code created by Pepijn Weitzel
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if 2 <= len(s) <= 6:
+        if s[0].isalpha() and s[1].isaplha():
+            return True
+    return False
+
+main()
