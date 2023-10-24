027fbedd1dc051df22a974386935123c49d74bff
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 16:45:47 +0000

    Tue, Oct 24, 2023, 6:45 PM +02:00

--- CS50p/outdated/outdated.py
+++ CS50p/outdated/outdated.py
@@ -1,1 +1,26 @@
 # Code created by Pepijn Weitzel
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+while True:
+    date = input("Date: ")
+    if len(date.split("/")) == 3:
+        info = date.split("/")
+    else:
+        print(date.split())
+
+
+    break
