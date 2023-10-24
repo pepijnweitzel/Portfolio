0dfcd0e81197e990ee95c1a5051f97b4307e393c
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 15:21:52 +0000

    Tue, Oct 24, 2023, 5:21 PM +02:00

--- CS50p/fuel/fuel.py
+++ CS50p/fuel/fuel.py
@@ -1,9 +1,36 @@
 # Code created by Pepijn Weitzel
 
 while True:
+    # Prompt user for fraction
     fraction = input("Fraction: ")
+    # Find out how many numbers before /
     for i in range(len(fraction)):
         if fraction[i] == "/":
             index = i
+    # Create x variable
+    x = ""
+    for i in range(index):
+        x += fraction[i]
+    # Create y variable
+    y = ""
+    for i in range(index + 1, len(fraction)):
+        y += fraction[i]
+
+    # Make sure x and y are integers
     try:
+        x = int(x)
+        y = int(y)
+    except ValueError:
+        continue
+
+    # Make sure y is bigger than x
+    if x > y:
+        continue
+
+    try:
+        z = float(x) / float(y)
+    except ZeroDivisionError:
+        continue
+    else:
+        z = round(z * 100)
         
