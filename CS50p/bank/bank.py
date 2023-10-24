dfd3c5aaef38ac7433302ac7d4ad766d43dc3807
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 10:33:43 +0000

    Tue, Oct 24, 2023, 12:33 PM +02:00

--- CS50p/bank/bank.py
+++ CS50p/bank/bank.py
@@ -1,9 +1,10 @@
 # Code created by Pepijn Weitzel
 # Prompt user for the greet
-greet = input("Greet: ").lower().strip()
-
+greet = input("Greet: ").lower().strip().strip(",")
+lister = greet.split()
+print(lister[0])
 # Check whether greet = hello or start with an H
-if greet == "hello":
+if greet == "hello" or lister[0] == "hello":
     print("$0")
 elif greet[0] == "h":
     print("$20")
