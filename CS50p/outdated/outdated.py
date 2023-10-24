55685fefd355864323d418d1d7c4f8f4d7ed1f55
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 16:57:21 +0000

    Tue, Oct 24, 2023, 6:57 PM +02:00

--- CS50p/outdated/outdated.py
+++ CS50p/outdated/outdated.py
@@ -21,8 +21,18 @@ while True:
 
     # Check which format it has used
     if len(date.split("/")) == 3:
+        # Get list of all dates
         info = date.split("/")
-        if len(info[1])
+        # Make all dates integers
+        for data in info:
+            data = int(data)
+        # Check whether dates are correct
+        if info[1] < 0 or info[1] > 31:
+            continue
+        if info[0] < 0 or info[1] > 12:
+            continue
+        print(f"{info[0]}-{info[1]}-{info[2]}")
+        break
 
 
     # Check which format it has used
