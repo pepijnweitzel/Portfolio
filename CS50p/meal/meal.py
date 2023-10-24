b6ea2ee05bd98a3dbe2cdd3aee6a1042e0216de3
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 11:06:07 +0000

    Tue, Oct 24, 2023, 1:06 PM +02:00

--- CS50p/meal/meal.py
+++ CS50p/meal/meal.py
@@ -1,1 +1,13 @@
 # Code created by Pepijn Weitzel
+def main():
+    time = input("What time is it? ")
+    convert(time)
+
+
+def convert(time):
+    if len(time == 4):
+    tmp = len[1]
+
+
+if __name__ == "__main__":
+    main()
