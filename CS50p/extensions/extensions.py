4a54b4e857d0e676090cedbf55e135f6ff6d9cb8
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 10:48:03 +0000

    Tue, Oct 24, 2023, 12:48 PM +02:00

--- CS50p/extensions/extensions.py
+++ CS50p/extensions/extensions.py
@@ -2,6 +2,21 @@
 # Prompt user for file name
 file = input("File name: ").lower()
 
+
 match file:
     case str(x) if ".gif" in x:
-        print("test")
+        print("image/gif")
+    case str(x) if ".jpg" in x:
+        print("image/jpg")
+    case str(x) if ".jpeg" in x:
+        print("image/jpeg")
+    case str(x) if ".png" in x:
+        print("image/png")
+    case str(x) if ".pdg" in x:
+        print("image/pdg")
+    case str(x) if ".txt" in x:
+        print("image/txt")
+    case str(x) if ".zip" in x:
+        print("image/zip")
+    case _:
+        print("application/octet-stream")
