7343c914f1f5625e0550e70a56ddf100f29d8466
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 11:26:45 +0000

    Tue, Oct 24, 2023, 1:26 PM +02:00

--- CS50p/meal/meal.py
+++ CS50p/meal/meal.py
@@ -1,15 +1,23 @@
 # Code created by Pepijn Weitzel
 def main():
     time = input("What time is it? ")
-    convert(time)
-
+    time = convert(time)
+    if 7.00 <= time <= 8.00:
+        print("breakfast time")
+    elif 12.00 <= time <= 13.00:
+        print("lunch time")
+    elif 18.00 <= time <= 19.00:
+        print("dinner time")
 
 def convert(time):
     if len(time) == 4:
         hour = time[0] + "."
-        minutes = int(time[2] + time[3])/60*100
-        print(hour, minutes)
-
+        minutes = str(int(time[2] + time[3])/60*100).split(".")[0]
+        return float(hour + minutes)
+    else:
+        hour = time[0] + time[1] + "."
+        minutes = str(int(time[3] + time[4])/60*100).split(".")[0]
+        return float(hour + minutes)
 
 if __name__ == "__main__":
     main()
