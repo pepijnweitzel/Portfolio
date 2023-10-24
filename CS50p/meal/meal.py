82814dc794d2f60ab2f57011e0394f7913e954a4
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 11:12:53 +0000

    Tue, Oct 24, 2023, 1:12 PM +02:00

--- CS50p/meal/meal.py
+++ CS50p/meal/meal.py
@@ -7,8 +7,18 @@ def main():
 def convert(time):
     if len(time == 4):
         tmp = len[0]
-        minutes = str(((int(len[2] + len[3]))/60)*100)
+        minutes = str((float(len[2] + len[3]))/60)
         tmp += minutes
+        if 7.00 <= tmp <= 8.00:
+            print("breakfast time")
+    else:
+        tmp = len[0] + len[1]
+        minutes = str((float(len[3] + len[4]))/60)
+        tmp += minutes
+        if 12.00 <= tmp <= 13.00:
+            print("lunch time")
+        elif 18.00 <= tmp <= 19.00:
+            print("dinner time")
 
 
 
