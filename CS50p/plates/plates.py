4dcb2bb2c6f40347aa82f91d4b310c90424d0997
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 12:40:27 +0000

    Tue, Oct 24, 2023, 2:40 PM +02:00

--- CS50p/plates/plates.py
+++ CS50p/plates/plates.py
@@ -9,9 +9,25 @@ def main():
 
 
 def is_valid(s):
+    # Check whether length is right
     if 2 <= len(s) <= 6:
-        if s[0].isalpha() and s[1].isaplha():
-            return True
+        # Check whether first 2 chars are letters
+        if s[0].isalpha() and s[1].isalpha():
+            # Check whether no letters after numbers
+            first_number = 0
+            for i in range(len(s)):
+                if s[i].isalpha() == False:
+                    first_number = i
+                    break
+            for i in range(i, len(s)):
+                if s[i].isalpha() == True:
+                    return False
+            # Check for periods, spaces or punctuation
+            for char in s:
+                if 
+
+
+
     return False
 
 main()
