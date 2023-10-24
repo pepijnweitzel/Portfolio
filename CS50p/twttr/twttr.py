7b8ccd3da995fae34bc6a6eb68cd1bd3831460f1
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 12:23:56 +0000

    Tue, Oct 24, 2023, 2:23 PM +02:00

--- CS50p/twttr/twttr.py
+++ CS50p/twttr/twttr.py
@@ -1,8 +1,8 @@
 # Code created by Pepijn Weitzel
-text = input("Input")
+text = input("Input: ")
 new_text = ""
 for letter in text:
-    if letter.lower() == "a" or letter.lower() == "e" or letter.lower() == "i" or letter.lower() == "o" or letter.lower() == "u"
+    if letter.lower() == "a" or letter.lower() == "e" or letter.lower() == "i" or letter.lower() == "o" or letter.lower() == "u":
         continue
     else:
         new_text += letter
