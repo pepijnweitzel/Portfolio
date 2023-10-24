796a1d03169416bfba1dda302b6867c5462dee73
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-24 10:44:42 +0000

    Tue, Oct 24, 2023, 12:44 PM +02:00

--- CS50p/extensions/extensions.py
+++ CS50p/extensions/extensions.py
@@ -1,4 +1,7 @@
 # Code created by Pepijn Weitzel
 # Prompt user for file name
-file = input("File name: ")
+file = input("File name: ").lower()
 
+match file:
+    case str(x) if ".gif" in x:
+        print("test")
