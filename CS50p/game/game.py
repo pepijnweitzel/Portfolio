9abf0a0624433e67db51f3cec27439fde9cdc700
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 10:52:13 +0000

    Wed, Oct 25, 2023, 12:52 PM +02:00

--- CS50p/game/game.py
+++ CS50p/game/game.py
@@ -3,7 +3,11 @@ import random
 
 
 def main():
-
+    while level < 0:
+        try:
+            level = int(input("Level"))
+        except ValueError:
+            break
 
 
 if __name__ == "__main__":
