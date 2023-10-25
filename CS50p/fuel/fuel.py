82b411626b00ac3794c126844f136e4f3a23d8aa
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 13:49:39 +0000

    Wed, Oct 25, 2023, 3:49 PM +02:00

--- CS50p/fuel/fuel.py
+++ CS50p/fuel/fuel.py
@@ -4,11 +4,36 @@ def main():
     while True:
         # Prompt user for fraction
         fraction = input("Fraction: ")
-
+        answer = convert(fraction)
 
 def convert(fraction):
-    ...
+    # Find out how many numbers before /
+        j = 0
+        for i in range(len(fraction)):
+            if fraction[i] == "/":
+                index = i
+                j = 1
 
+        # Make sure / symbol was given
+        if j != 1:
+            continue
+
+        # Create x variable
+        x = ""
+        for i in range(index):
+            x += fraction[i]
+        # Create y variable
+        y = ""
+        for i in range(index + 1, len(fraction)):
+            y += fraction[i]
+
+        # Make sure x and y are integers
+        try:
+            x = int(x)
+            y = int(y)
+        except ValueError:
+            raise ValueError
+            continue
 
 def gauge(percentage):
     ...
@@ -18,34 +43,6 @@ if __name__ == "__main__":
     main()
 
 while True:
-    # Prompt user for fraction
-    fraction = input("Fraction: ")
-    # Find out how many numbers before /
-    j = 0
-    for i in range(len(fraction)):
-        if fraction[i] == "/":
-            index = i
-            j = 1
-
-    # Make sure / symbol was given
-    if j != 1:
-        continue
-
-    # Create x variable
-    x = ""
-    for i in range(index):
-        x += fraction[i]
-    # Create y variable
-    y = ""
-    for i in range(index + 1, len(fraction)):
-        y += fraction[i]
-
-    # Make sure x and y are integers
-    try:
-        x = int(x)
-        y = int(y)
-    except ValueError:
-        continue
 
     # Make sure y is bigger than x
     if x > y:
