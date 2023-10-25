da965d481e33060d80ae87843e42832b81981703
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 12:57:03 +0000

    Wed, Oct 25, 2023, 2:57 PM +02:00

--- CS50p/test_twttr/test_twttr.py
+++ CS50p/test_twttr/test_twttr.py
@@ -1,1 +1,13 @@
 # Code created by Pepijn Weitzel
+import twttr
+
+def main():
+    print(shorten("test"))
+
+
+def test_shorten():
+    assert shorten("Kaas") == "Ks"
+    assert shorten("") == ""
+    assert shorten("bdaeioudb") == "bddb"
+
+main()
