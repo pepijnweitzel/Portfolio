fc998bb8d1f1ca8f9ce3045c7170fd1355807d49
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:08:37 +0000

    Wed, Oct 25, 2023, 1:08 PM +02:00

--- CS50p/professor/professor.py
+++ CS50p/professor/professor.py
@@ -4,15 +4,25 @@ import random
 
 
 def main():
-    ...
+    level = get_level()
 
 
 def get_level():
-    ...
+    while True:
+        try:
+            level = int(input("Level: "))
+        except ValueError:
+            continue
+        else:
+            if level in range(1, 4):
+                return level
+            else:
+                continue
 
 
 def generate_integer(level):
-    ...
+    if level == 1:
+        
 
 
 if __name__ == "__main__":
