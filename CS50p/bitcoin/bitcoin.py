b669815b00d6ca81a972b800800a92bd77d93673
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:32:20 +0000

    Wed, Oct 25, 2023, 1:32 PM +02:00

--- CS50p/bitcoin/bitcoin.py
+++ CS50p/bitcoin/bitcoin.py
@@ -1,1 +1,18 @@
 # Code created by Pepijn Weitzel
+import requests
+import sys
+
+def main():
+    # Check for right input
+    if sys.argv != 2:
+        sys.exit("Missing command-line argument")
+    try:
+        number = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+
+    
+
+
+if __name__ == "__main__":
+    main()
