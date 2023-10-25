722a35f45d49bbb5e2601e7ef5f57729345c266c
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 10:21:30 +0000

    Wed, Oct 25, 2023, 12:21 PM +02:00

--- CS50p/figlet/figlet.py
+++ CS50p/figlet/figlet.py
@@ -1,6 +1,22 @@
 # Code created by Pepijn Weitzel
 from pyfiglet import Figlet
+import sys
+from random import choice
 
 figlet = Figlet()
 
-print(figlet.getFonts())
+all_fonts = figlet.getFonts()
+
+# Check for errors
+if len(sys.argv) != 3:
+    sys.exit("Invalid usage")
+elif sys.argv[2] not in all_fonts:
+    sys.exit("Invalid usage")
+elif sys.argv[1] != "-f" and sys.argv[1] != "--font":
+    sys.exit("Invalid usage")
+else:
+    set_font = choice(all_fonts)
+    figlet.setFont(font=set_font)
+    text = input("Input: ")
+    print("Output:")
+    print(figlet.renderText(text))
