28d6278b8f60ae74fadf32d3ec31873b86cd1bea
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 13:23:42 +0000

    Wed, Oct 25, 2023, 3:23 PM +02:00

--- CS50p/bank/test_bank.py
+++ CS50p/bank/test_bank.py
@@ -1,2 +1,11 @@
 # Code created by Pepijn Weitzel
-from bank import
+from bank import value
+
+def test_hello():
+    assert value("hello") == 0
+
+def test_start_h():
+    assert value("hey") == 20
+
+def test_nothing():
+    assert value("bye") == 100
