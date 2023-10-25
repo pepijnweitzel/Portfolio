b60b2cbe2c97c4ac0b152b0608b5942f6282d378
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:36:29 +0000

    Wed, Oct 25, 2023, 1:36 PM +02:00

--- CS50p/bitcoin/bitcoin.py
+++ CS50p/bitcoin/bitcoin.py
@@ -1,17 +1,19 @@
 # Code created by Pepijn Weitzel
 import requests
 import sys
+import json
 
 def main():
     # Check for right input
-    if sys.argv != 2:
+    if len(sys.argv) != 2:
         sys.exit("Missing command-line argument")
     try:
         number = float(sys.argv[1])
     except ValueError:
         sys.exit("Command-line argument is not a number")
 
-    
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    print(response)
 
 
 if __name__ == "__main__":
