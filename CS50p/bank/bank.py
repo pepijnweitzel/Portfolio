992c7616f0a5a9af2901a9a2eadb6cc68799ea06
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 13:21:35 +0000

    Wed, Oct 25, 2023, 3:21 PM +02:00

--- CS50p/bank/bank.py
+++ CS50p/bank/bank.py
@@ -1,10 +1,20 @@
 # Code created by Pepijn Weitzel
-# Prompt user for the greet
-greet = input("Greet: ").lower()
-# Check whether greet = hello or start with an H
-if "hello" in greet:
-    print("$0")
-elif greet[0] == "h":
-    print("$20")
-else:
-    print("$100")
+
+def main():
+    # Prompt user for the greet
+    greet = input("Greet: ").lower()
+    money = value(greet)
+    print(f"${money}")
+
+def value(greeting):
+    # Check whether greet = hello or start with an H
+    if "hello" in greeting:
+        return 0
+    elif greeting[0] == "h":
+        return 20
+    else:
+        return 100
+
+
+if __name__ == "__main__":
+    main()
