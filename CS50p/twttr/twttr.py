25890a0d352a2509b103718d74355f25e13b66c2
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 12:48:49 +0000

    Wed, Oct 25, 2023, 2:48 PM +02:00

--- CS50p/twttr/twttr.py
+++ CS50p/twttr/twttr.py
@@ -1,9 +1,15 @@
 # Code created by Pepijn Weitzel
-text = input("Input: ")
-new_text = ""
-for letter in text:
-    if letter.lower() == "a" or letter.lower() == "e" or letter.lower() == "i" or letter.lower() == "o" or letter.lower() == "u":
-        continue
-    else:
-        new_text += letter
-print(f"Output: {new_text}")
+
+def main():
+    text = input("Input: ")
+    text = shorten(text)
+    print(f"Output: {text}")
+
+def shorten(string):
+    new_text = ""
+    for letter in string:
+        if letter.lower() == "a" or letter.lower() == "e" or letter.lower() == "i" or letter.lower() == "o" or letter.lower() == "u":
+            continue
+        else:
+            new_text += letter
+    return new_text
