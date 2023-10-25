8c202b9474307380cecca290607bc74255695de3
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:00:53 +0000

    Wed, Oct 25, 2023, 1:00 PM +02:00

--- CS50p/game/game.py
+++ CS50p/game/game.py
@@ -3,14 +3,33 @@ import random
 
 
 def main():
-    level = -1
-    while level < 0:
+    while True:
         try:
             level = int(input("Level: "))
-            break
+            if level > 0:
+                break
+            else:
+                 continue
         except ValueError:
-            continue
-    print(level)
+                continue
+    # user gave right input:
+    number = random.randint(1, level)
+
+    while True:
+        try:
+            guess = int(input("Guess: "))
+        except ValueError:
+             continue
+        else:
+             if guess > number:
+                  print("Too large!")
+                  continue
+             elif guess < number:
+                  print("Too small!")
+                  continue
+             else:
+                  print("Just right!")
+                  break
 
 
 if __name__ == "__main__":
