07e1895383ac89b3fc47a617863fd075ae1a67f4
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 14:01:36 +0000

    Wed, Oct 25, 2023, 4:01 PM +02:00

--- CS50p/fuel/test_fuel.py
+++ CS50p/fuel/test_fuel.py
@@ -6,10 +6,11 @@ def test_convert():
     assert convert("12/24") == 50
     with pytest.raises(ValueError):
         convert("24/12")
-        convert("12/0")
         convert("pe/at")
+    with pytest.raises(ZeroDivisionError):
+        convert("12/0")
 
 def test_gauge():
     assert gauge(1) == "E"
-    assert gauge(100) == "F"
+    assert gauge(99) == "F"
     assert gauge(23) == "23%"
