9c5936207d2321649ad2628bb91859db53d17009
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:41:20 +0000

    Wed, Oct 25, 2023, 1:41 PM +02:00

--- CS50p/bitcoin/bitcoin.py
+++ CS50p/bitcoin/bitcoin.py
@@ -12,9 +12,12 @@ def main():
     except ValueError:
         sys.exit("Command-line argument is not a number")
 
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print(response)
-
+    try:
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    except requests.RequestException:
+        sys.exit("Request error")
+    else:
+        print(response.json())
 
 if __name__ == "__main__":
     main()
