00a6c1b1dd6176d5b77e1aa3b29a447d7c8ef8a9
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-25 11:03:36 +0000

    Wed, Oct 25, 2023, 1:03 PM +02:00

---
+++ CS50p/professor/professor.py
@@ -0,0 +1,19 @@
+# Code created by Pepijn Weitzel
+
+import random
+
+
+def main():
+    ...
+
+
+def get_level():
+    ...
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
