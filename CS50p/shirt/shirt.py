d28a685c53849870832adf56bd766c62d2e1c616
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 14:03:54 +0000

    Thu, Oct 26, 2023, 4:03 PM +02:00

--- CS50p/shirt/shirt.py
+++ CS50p/shirt/shirt.py
@@ -1,9 +1,12 @@
 # Code created by Pepijn Weitzel
 import sys
 
+# Check for errors
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif sys.argv[1][len(sys.argv[1])-5] != ".jpeg" or sys.argv[1][len(sys.argv[1])-4] != ".jpg" or sys.argv[1][len(sys.argv[1])-4] != ".png":
-    sys.exit("")
+elif sys.argv[2][len(sys.argv[2])-5:] != ".jpeg" or sys.argv[2][len(sys.argv[2])-4:] != ".jpg" or sys.argv[2][len(sys.argv[2])-4:] != ".png":
+    sys.exit("Invalid output")
+elif sys.argv[1][len(sys.argv[1])-4:] != sys.argv[2][len(sys.argv[2])-4:]:
+    sys.exit("Input and output have different extensions")
