2de15dc25f3525464e0f6cf29e836af254524906
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 14:15:47 +0000

    Thu, Oct 26, 2023, 4:15 PM +02:00

--- CS50p/shirt/shirt.py
+++ CS50p/shirt/shirt.py
@@ -8,15 +8,18 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif sys.argv[2][len(sys.argv[2])-5:] != ".jpeg" or sys.argv[2][len(sys.argv[2])-4:] != ".jpg" or sys.argv[2][len(sys.argv[2])-4:] != ".png":
-    print()
+elif sys.argv[2][len(sys.argv[2])-5:] != ".jpeg" and sys.argv[2][len(sys.argv[2])-4:] != ".jpg" and sys.argv[2][len(sys.argv[2])-4:] != ".png":
     sys.exit("Invalid output")
 elif sys.argv[1][len(sys.argv[1])-4:] != sys.argv[2][len(sys.argv[2])-4:]:
     sys.exit("Input and output have different extensions")
 
 # Open and save images
 images = []
-image = Image.open(sys.argv[1])
-images.append(image)
-image = Image.open("shirt.png")
-images.append(image)
+try:
+    image = Image.open(sys.argv[1])
+except FileNotFoundError:
+    sys.exit("Input does not exist")
+else:
+    images.append(image)
+    image = Image.open("shirt.png")
+    images.append(image)
