d414450ea20b50e1a24ebd4887165b3bc453a10f
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 14:33:36 +0000

    Thu, Oct 26, 2023, 4:33 PM +02:00

--- CS50p/shirt/shirt.py
+++ CS50p/shirt/shirt.py
@@ -13,20 +13,19 @@ elif sys.argv[2][len(sys.argv[2])-5:] != ".jpeg" and sys.argv[2][len(sys.argv[2]
 elif sys.argv[1][len(sys.argv[1])-4:] != sys.argv[2][len(sys.argv[2])-4:]:
     sys.exit("Input and output have different extensions")
 
-# Open and save images
-images = []
+# Open images
 try:
     image = Image.open(sys.argv[1])
 except FileNotFoundError:
     sys.exit("Input does not exist")
 else:
-    images.append(image)
     shirt = Image.open("shirt.png")
 
-# Size
-image_size = image.size
-image = ImageOps.fit(image, image_size)
+# Make image same size as shirt
+image = ImageOps.fit(image, shirt.size)
 
+# Paste shirt onto image
 image.paste(shirt, shirt)
 
+# Save new image with given filename
 image.save(sys.argv[2], save=True)
