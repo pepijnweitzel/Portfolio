fb15bee233aaaf7655f6e4d9caaf44c57be83aed
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 13:08:05 +0000

    Thu, Oct 26, 2023, 3:08 PM +02:00

--- CS50p/scourgify/scourgify.py
+++ CS50p/scourgify/scourgify.py
@@ -5,4 +5,9 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
+try:
+    file = open(sys.argv[1])
+    file.close()
+except FileNotFoundError:
+    sys.exit("Could not read ", sys.argv[1])
 
