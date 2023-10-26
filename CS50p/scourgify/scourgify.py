c82a7b98f8ae5974f5fc9e4a8da9e0a0280f22b5
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 12:58:17 +0000

    Thu, Oct 26, 2023, 2:58 PM +02:00

---
+++ CS50p/scourgify/scourgify.py
@@ -0,0 +1,8 @@
+# Code created by Pepijn Weitzel
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+
