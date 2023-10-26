bded0b6ab0abab34dedf35f2b9ab9230b13f1aad
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 13:11:19 +0000

    Thu, Oct 26, 2023, 3:11 PM +02:00

--- CS50p/scourgify/scourgify.py
+++ CS50p/scourgify/scourgify.py
@@ -1,5 +1,6 @@
 # Code created by Pepijn Weitzel
 import sys
+import csv
 
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
@@ -11,3 +12,11 @@ try:
 except FileNotFoundError:
     sys.exit("Could not read ", sys.argv[1])
 
+
+some = []
+with open(sys.argv[1]) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        some.append(row)
+
+print(some)
