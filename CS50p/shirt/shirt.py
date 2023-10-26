8383fff8ace4b711e53719d3e9c9c56a3618be99
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-26 14:01:14 +0000

    Thu, Oct 26, 2023, 4:01 PM +02:00

--- CS50p/shirt/shirt.py
+++ CS50p/shirt/shirt.py
@@ -1,1 +1,9 @@
 # Code created by Pepijn Weitzel
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+elif sys.argv[1][len(sys.argv[1])-5] != ".jpeg" or sys.argv[1][len(sys.argv[1])-4] != ".jpg" or sys.argv[1][len(sys.argv[1])-4] != ".png":
+    sys.exit("")
