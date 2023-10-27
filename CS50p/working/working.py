0e60dd624e087568d2409e7b9d20c67fc04f002d
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:44:04 +0000

    Fri, Oct 27, 2023, 1:44 PM +02:00

--- CS50p/working/working.py
+++ CS50p/working/working.py
@@ -10,10 +10,10 @@ def main():
 def convert(s):
 
     if time := re.search(r"((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM)) to ((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM))", s):
-        if time.group(2) == "AM":
-            start = time.group(1)
-        else:
-            start == str(int(time.group(1)) + 12)
+        # Check for which format was used
+        if ":" in time.group(1):
+            hours, minutes = time.group(1).split(":")
+            
 
         return start
     else:
