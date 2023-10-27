4f368476a46ad466504f2f109d8b97c2cbe66a2e
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:38:52 +0000

    Fri, Oct 27, 2023, 1:38 PM +02:00

--- CS50p/working/working.py
+++ CS50p/working/working.py
@@ -9,10 +9,14 @@ def main():
 
 def convert(s):
 
-    if time := re.search(r"([0-9](?::[0-5][0-9])?|1[0-2])"):
+    if time := re.search(r"((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM)) to ((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM))", s):
+        print(time.group(1))
+        print(time.group(2))
+        print(time.group(3))
+        print(time.group(4))
         return time
-
-    raise ValueError
+    else:
+        raise ValueError
 
 
 
