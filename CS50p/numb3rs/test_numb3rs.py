74179a214d86cf8e0c440cd1c2a933d0147084ec
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 10:21:21 +0000

    Fri, Oct 27, 2023, 12:21 PM +02:00

---
+++ CS50p/numb3rs/test_numb3rs.py
@@ -0,0 +1,16 @@
+# Code created by Pepijn Weitzel
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
