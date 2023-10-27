8404ca43ac60c2a787ebd1de49a31e830f06dc15
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 13:08:00 +0000

    Fri, Oct 27, 2023, 3:08 PM +02:00

--- CS50p/working/test_working.py
+++ CS50p/working/test_working.py
@@ -1,6 +1,20 @@
 # Code created by Pepijn Weitzel
 
 from working import convert
+import pytest
 
 
-def test_
+def test_wrong_value():
+    with pytest.raises(ValueError):
+        convert("9AM - 5PM")
+        convert(" to ")
+        convert("13 PM to 18 AM")
+
+def test_correct():
+    assert convert("9 AM to 5 PM") == "9:00 to 17:00"
+    assert convert("12 AM to 5 PM") == "00:00 to 17:00"
+    assert convert("4 AM to 12 PM") == "4:00 to 12:00"
+
+def test_correct_colon():
+    assert convert("9:30 AM to 5:30 PM") == "9:30 to 17:30"
+    assert convert("7:30 AM to 2:30 PM") == "7:30 to 14:30"
