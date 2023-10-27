eb6d4074e7f01116f23cf16b7b0bd0be8f0a3aac
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 14:00:14 +0000

    Fri, Oct 27, 2023, 4:00 PM +02:00

--- CS50p/response/response.py
+++ CS50p/response/response.py
@@ -1,10 +1,16 @@
 # Code created by Pepijn Weitzel
 
+from validator_collection import validators, errors
+
 def main():
     email = input("What's you email address? ")
 
-
-
+    try:
+        email = validators.email(email)
+    except errors.InvalidEmailError:
+        print("Invalid")
+    else:
+        print("Valid")
 
 if __name__ == "__main__":
     main()
