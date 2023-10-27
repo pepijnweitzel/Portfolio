7d791515904c07e23067ce5a44aa563c2f344290
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:14:03 +0000

    Fri, Oct 27, 2023, 1:14 PM +02:00

--- CS50p/watch/watch.py
+++ CS50p/watch/watch.py
@@ -11,9 +11,16 @@ def main():
 def parse(s):
 
     if link := re.search(r'^<iframe(?:.)+src="(https?://(?:www\.)?youtube\.com)/embed(/.+)"></iframe>$', s):
+        # If string contains https
+        if re.search(r".+https.+", s):
+            youtube_link = link.group(1) + link.group(2)
+            return youtube_link
+        # If string contains http
+        else:
+            link.group(1) = re.sub(r"http", "https", link.group(1))
+            youtube_link = link.group(1) + link.group(2)
+            return youtube_link
 
-        youtube_link = link.group(1) + link.group(3)
-        return youtube_link
     else:
         return "None"
 
