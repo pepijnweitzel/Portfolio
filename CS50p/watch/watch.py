232449648fe731e735af8ffb7426e1efe7090d87
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:21:22 +0000

    Fri, Oct 27, 2023, 1:21 PM +02:00

--- CS50p/watch/watch.py
+++ CS50p/watch/watch.py
@@ -14,12 +14,14 @@ def parse(s):
         # If string contains https
         if re.search(r".+https.+", s):
             new_youtube = re.sub(r"youtube\.com", "youtu.be", link.group(1))
+            new_youtube = re.sub(r"www\.", "", new_youtube)
             youtube_link = new_youtube + link.group(2)
             return youtube_link
         # If string contains http
         else:
             new_string = re.sub(r"http", "https", link.group(1))
             new_youtube = re.sub(r"youtube\.com", "youtu.be", new_string)
+            new_youtube = re.sub(r"www\.", "", new_youtube)
             youtube_link = new_youtube + link.group(2)
             return youtube_link
 
