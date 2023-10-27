871d0f0220ca3e7c179fcd49568196f4afc31fe0
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:42:08 +0000

    Fri, Oct 27, 2023, 1:42 PM +02:00

--- CS50p/working/working.py
+++ CS50p/working/working.py
@@ -10,11 +10,12 @@ def main():
 def convert(s):
 
     if time := re.search(r"((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM)) to ((?:[0-9](?::[0-5][0-9])?)|(?:1[0-2])(?::[0-5][0-9])?) ((?:AM)|(?:PM))", s):
-        print(time.group(1))
-        print(time.group(2))
-        print(time.group(3))
-        print(time.group(4))
-        return time
+        if time.group(2) == "AM":
+            start = time.group(1)
+        else:
+            start == str(int(time.group(1)) + 12)
+
+        return start
     else:
         raise ValueError
 
