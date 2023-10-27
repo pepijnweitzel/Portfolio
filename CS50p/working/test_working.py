0dab5fdb5dd955a54b1121c3cb125bc7fb214c05
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 13:10:09 +0000

    Fri, Oct 27, 2023, 3:10 PM +02:00

--- CS50p/working/test_working.py
+++ CS50p/working/test_working.py
@@ -9,12 +9,14 @@ def test_wrong_value():
         convert("9AM - 5PM")
         convert(" to ")
         convert("13 PM to 18 AM")
+        convert("12 PM 2 AM")
+        convert
 
 def test_correct():
-    assert convert("9 AM to 5 PM") == "9:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("12 AM to 5 PM") == "00:00 to 17:00"
-    assert convert("4 AM to 12 PM") == "4:00 to 12:00"
+    assert convert("4 AM to 12 PM") == "04:00 to 12:00"
 
 def test_correct_colon():
-    assert convert("9:30 AM to 5:30 PM") == "9:30 to 17:30"
-    assert convert("7:30 AM to 2:30 PM") == "7:30 to 14:30"
+    assert convert("9:30 AM to 5:30 PM") == "09:30 to 17:30"
+    assert convert("7:30 AM to 2:30 PM") == "07:30 to 14:30"
