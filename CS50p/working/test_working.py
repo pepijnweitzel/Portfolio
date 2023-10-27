0c64a64287e36902cde4a5fde58cbd9b5746c0b7
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 11:28:12 +0000

    Fri, Oct 27, 2023, 1:28 PM +02:00

---
+++ CS50p/working/test_working.py
@@ -0,0 +1,1 @@
+# Code created by Pepijn Weitzel
---
+++ CS50p/working/working.py
@@ -0,0 +1,17 @@
+# Code created by Pepijn Weitzel
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    return s
+
+
+
+
+if __name__ == "__main__":
+    main()
