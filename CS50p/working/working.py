1c7dcf1c1590b961cf7c66cc3a1c51e8f6e43466
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 12:03:13 +0000

    Fri, Oct 27, 2023, 2:03 PM +02:00

--- CS50p/working/working.py
+++ CS50p/working/working.py
@@ -21,6 +21,8 @@ def convert(s):
             else:
                 if hours == "12":
                     hours = "00"
+                elif 0 <= int(hours) <= 9:
+                    hours = "0" + hours
             start = f"{hours}:{minutes}"
         else:
             if time.group(2) == "PM":
@@ -38,6 +40,8 @@ def convert(s):
             else:
                 if hours == "12":
                     hours = "00"
+                elif 0 <= int(hours) <= 9:
+                    hours = "0" + hours
             end = f"{hours}:{minutes}"
         else:
             if time.group(4) == "PM":
