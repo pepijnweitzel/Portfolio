db53c07ce47d5f16f2ae4835534ecf7aefe133b2
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 13:31:48 +0000

    Fri, Oct 27, 2023, 3:31 PM +02:00

--- CS50p/um/um.py
+++ CS50p/um/um.py
@@ -10,12 +10,9 @@ def main():
 
 def count(s):
     count = 0
-    if string := re.search(r"^(um[,\.?!\s])?(\sum[\s,.!?\n])", s, re.IGNORECASE):
+    if string := re.search(r"([\s]um[\s,\.])", s, re.IGNORECASE):
         print(string.group(1))
-        print(string.group(2))
-        for char in string.group(2):
-            if char.lower() == "u":
-                count += 1
+
 
     return count
 
