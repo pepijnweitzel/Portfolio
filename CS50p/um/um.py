9c9870fa51270c63ca898b5fa0e37644d1b8d355
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-27 13:13:54 +0000

    Fri, Oct 27, 2023, 3:13 PM +02:00

---
+++ CS50p/um/um.py
@@ -0,0 +1,19 @@
+# Code created by Pepijn Weitzel
+
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
