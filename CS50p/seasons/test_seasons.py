cdc2c39ad4fd10c5dec6c076b0794160b5e0a3a3
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-28 14:49:41 +0000

    Sat, Oct 28, 2023, 4:49 PM +02:00

--- CS50p/seasons/test_seasons.py
+++ CS50p/seasons/test_seasons.py
@@ -1,1 +1,16 @@
 # Code created by Pepijn Weitzel
+
+from seasons import get_difference, get_text
+import datetime
+
+
+
+
+def test_get_difference():
+    assert get_difference(datetime.date.today()) == 0
+    assert get_difference(datetime.date(2023, 10, 27)) == 1440
+
+
+def test_get_text():
+    assert get_text(0) == "Zero minutes"
+    assert get_text(10) == "Ten minutes"
