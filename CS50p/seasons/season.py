e7a680d2da5d89d14e7f4e7aadf7a4ca51bf74d8
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-28 13:52:55 +0000

    Sat, Oct 28, 2023, 3:52 PM +02:00

--- CS50p/seasons/season.py
+++ CS50p/seasons/season.py
@@ -1,13 +1,24 @@
 # Code created by Pepijn Weitzel
 
 from datetime import date
+import re
+import sys
 
 
 def main():
-    ...
+    given_date = get_date()
+    print(given_date)
+    print(type(date.today()))
 
+def get_date():
 
-...
+    string = input("Date of Birth: ")
+    if string:= re.search(r"^([0-9]{4})-([0-9]{2})-([0-9]{2})", string):
+        if  1 <= int(string.group(2)) <= 12:
+            if 1 <= int(string.group(3)) <= 30:
+
+                    return [string.group(1), string.group(2), string.group(3)]
+    sys.exit("Invalid date")
 
 
 if __name__ == "__main__":
