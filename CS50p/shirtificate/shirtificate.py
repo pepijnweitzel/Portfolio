01709c7bdabdbf18aff5a23223e0e2e39792afba
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 14:47:54 +0000

    Sun, Oct 29, 2023, 3:47 PM +01:00

--- CS50p/shirtificate/shirtificate.py
+++ CS50p/shirtificate/shirtificate.py
@@ -6,7 +6,7 @@ def main():
     # Prompt user for name
     name = input("Name: ")
 
-    pdf = FPDF()
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
     pdf.cell(40, 10, name)
