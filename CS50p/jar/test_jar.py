5f6ee4ed6cf749f2af0b6305a1475dfec5318db1
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 12:54:01 +0000

    Sun, Oct 29, 2023, 1:54 PM +01:00

--- CS50p/jar/test_jar.py
+++ CS50p/jar/test_jar.py
@@ -6,11 +6,18 @@ import pytest
 def test_init():
     with pytest.raises(ValueError):
         Jar("cat")
-    jar = Jar()
-    assert jar.capacity() == "12"
 
 def test_str():
     jar = Jar()
     assert str(jar) == ""
     jar.deposit(5)
     assert str(jar) == "🍪🍪🍪🍪🍪"
+    jar.withdraw(2)
+    assert str(jar) == "🍪🍪🍪"
+
+def test_deposit():
+    jar = Jar()
+    
+
+def test_withdraw():
+    ...
