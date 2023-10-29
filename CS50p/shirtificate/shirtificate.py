be38a7e12bc5d975ffb87d0f9206e4be710baea5
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 14:46:58 +0000

    Sun, Oct 29, 2023, 3:46 PM +01:00

--- CS50p/shirtificate/shirtificate.py
+++ CS50p/shirtificate/shirtificate.py
@@ -1,9 +1,16 @@
 # Code created by Pepijn Weitzel
+from fpdf import FPDF
 
 def main():
 
     # Prompt user for name
     name = input("Name: ")
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 16)
+    pdf.cell(40, 10, name)
+    pdf.output("tuto1.pdf")
 
 
 
