4abadbc2238e9bfbf6006f20605bbede3676bd60
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 15:22:18 +0000

    Sun, Oct 29, 2023, 4:22 PM +01:00

--- CS50p/shirtificate/shirtificate.py
+++ CS50p/shirtificate/shirtificate.py
@@ -13,8 +13,30 @@ class PDF(FPDF):
         # Printing title:
         self.cell(30, 10, "CS50 Shirtificate", align="C")
 
-# Instantiation of inherited class
-pdf = PDF()
-pdf.add_page()
-pdf.image("shirtificate.png", 10, 60, 190)
-pdf.output("shirtificate.pdf")
+    def body(self):
+        # Import image
+        self.image("shirtificate.png", 10, 60, 190)
+
+    def printing(self, name):
+        # Print name on shirt
+        self.ln(100)
+        self.set_font("helvetica", "B", 25)
+        self.cell(210, 10, f"{name} took CS50", align="C")
+
+def main():
+    # Create file and set body and header
+    pdf = PDF()
+    pdf.add_page()
+    pdf.body()
+
+    name = input("Name: ")
+
+    pdf.printing(name)
+
+
+    # Output the file
+    pdf.output("shirtificate.pdf")
+
+
+if __name__ == "__main__":
+    main()
