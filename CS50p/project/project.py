9da1bf182646504a1212d8e3f77b376ee8d90ea3
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 19:50:08 +0000

    Sun, Oct 29, 2023, 8:50 PM +01:00

--- CS50p/project/project.py
+++ CS50p/project/project.py
@@ -11,6 +11,14 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
 
 def function_1():
     ...
