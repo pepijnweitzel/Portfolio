844b2fde983005864fce680f006f61e2e42403ae
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 15:04:41 +0000

    Sun, Oct 29, 2023, 4:04 PM +01:00

--- CS50p/shirtificate/shirtificate.py
+++ CS50p/shirtificate/shirtificate.py
@@ -8,10 +8,8 @@ class PDF(FPDF):
         #self.image("shirtificate.png", 10, 8, 33)
         # Setting font: helvetica bold 15
         self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
         # Printing title:
-        self.text(30, 10, "CS50 Shirtificate")
+        self.cell(30, 10, "CS50 Shirtificate", align="C")
 
 # Instantiation of inherited class
 pdf = PDF()
