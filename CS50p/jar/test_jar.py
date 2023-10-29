5c3f174321d8aa8fd526699d02a6098d4273658f
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 12:52:36 +0000

    Sun, Oct 29, 2023, 1:52 PM +01:00

--- CS50p/jar/test_jar.py
+++ CS50p/jar/test_jar.py
@@ -1,1 +1,16 @@
 # Code created by Pepijn Weitzel
+
+from jar import Jar
+import pytest
+
+def test_init():
+    with pytest.raises(ValueError):
+        Jar("cat")
+    jar = Jar()
+    assert jar.capacity() == "12"
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(5)
+    assert str(jar) == "🍪🍪🍪🍪🍪"
