e0270fa12056578da960a1bfee67f5b5c6948f39
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-10-29 14:53:03 +0000

    Sun, Oct 29, 2023, 3:53 PM +01:00

--- CS50p/shirtificate/shirtificate.py
+++ CS50p/shirtificate/shirtificate.py
@@ -9,8 +9,8 @@ def main():
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
-    pdf.cell(40, 10, name)
-    pdf.output("tuto1.pdf")
+    pdf.cell(40, 50, name)
+    pdf.output("shirtificate.pdf")
 
 
 
