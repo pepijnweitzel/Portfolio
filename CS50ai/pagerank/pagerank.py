f779815d7a5f11ecdc625a0c643b4f006203e73d
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-11-03 10:49:16 +0000

    Fri, Nov 3, 2023, 11:49 AM +01:00

--- CS50ai/pagerank/pagerank.py
+++ CS50ai/pagerank/pagerank.py
@@ -114,6 +114,7 @@ def sample_pagerank(corpus, damping_factor, n):
     for _ in range(n):
         pb = transition_model(corpus, page, damping_factor)
         # pb looks like {1.html : 0.234, 2.html : 0.234, 3.html : 0.532}
+        # all_pages looks like [2.html, 3.html, 1.html, 4.html]
         weights = ()
         page = random.choices(all_pages, weights=weights, )
 
