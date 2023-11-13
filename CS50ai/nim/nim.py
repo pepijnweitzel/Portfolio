522c19b7b3aa3680f79a384355f9b8b1242f6c4b
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-11-13 15:50:46 +0000

    Mon, Nov 13, 2023, 4:50 PM +01:00

--- CS50ai/nim/nim.py
+++ CS50ai/nim/nim.py
@@ -101,6 +101,7 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
+        # Check if Q-value exists, if so return its value, otherwise return 0
         if self.q[state, action]:
             return self.q[state, action]
         else:
