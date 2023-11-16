d201b49a517abf3f3f418585c854f5e9ea416d66
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-11-16 19:39:07 +0000

    Thu, Nov 16, 2023, 8:39 PM +01:00

--- CS50ai/parser/parser.py
+++ CS50ai/parser/parser.py
@@ -62,13 +62,15 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
+    nltk.download('punkt')
 
-
+    # Set regular expression for tokenizer to only allow words not punctuation or numbers
     tokenizer = nltk.RegexpTokenizer(r'[a-zA-Z]+')
 
     # Create list of every word
     words = tokenizer.tokenize(sentence)
 
+    # Lowercase all words
 
 
     print(words)
