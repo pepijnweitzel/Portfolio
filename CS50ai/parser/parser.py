5dc73cb894df0e2e18f591670fd39eff8cbd7018
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-11-17 11:24:27 +0000

    Fri, Nov 17, 2023, 12:24 PM +01:00

--- CS50ai/parser/parser.py
+++ CS50ai/parser/parser.py
@@ -86,8 +86,11 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    print(tree)
-    print(len(tree))
+    print(tree[0], tree[1])
+    for i in range(len(tree)):
+        if tree[i] == "NP":
+            print("test")
+
     return []
 
 
