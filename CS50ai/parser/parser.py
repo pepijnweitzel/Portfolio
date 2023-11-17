05c9a9c1f296c9071e202580cf44a17e305f4a36
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-11-17 11:31:03 +0000

    Fri, Nov 17, 2023, 12:31 PM +01:00

--- CS50ai/parser/parser.py
+++ CS50ai/parser/parser.py
@@ -17,8 +17,9 @@ V -> "smiled" | "tell" | "were"
 NONTERMINALS = """
 S -> NP VP | NP VP VP
 
-NP -> NP NP| N | Det NP | P NP | Adj NP | Conj NP
+NP -> NP NP| N | Det NP | P NP | AD N | Conj NP
 VP -> V | V NP | NP V | Adv VP | VP Adv | Conj VP
+AD -> Adj | AD Adj
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
