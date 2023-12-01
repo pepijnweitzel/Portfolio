0af2c0559b1a9b5fb1374f0eda285cbdb61a1266
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-01 12:38:31 +0000

    Fri, Dec 1, 2023, 1:38 PM +01:00

--- CS50w/wiki/encyclopedia/urls.py
+++ CS50w/wiki/encyclopedia/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("<str:input>", views.search, name="search"),
-    path("new", views.new, name="new")
+    path("page/new", views.new, name="new")
 ]
