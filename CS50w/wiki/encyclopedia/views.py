29c6b10c157aba090ec97b55dd23efa27c236df5
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-04 13:03:29 +0000

    Mon, Dec 4, 2023, 2:03 PM +01:00

--- CS50w/wiki/encyclopedia/views.py
+++ CS50w/wiki/encyclopedia/views.py
@@ -45,7 +45,11 @@ def index(request):
 
 def entry(request, title):
 
+    # Get entries
     entries = util.list_entries()
+
+    # Pick a random page
+    random_page = [random.choice(entries)]
 
     # Check if page exists
     if title in entries:
@@ -58,6 +62,7 @@ def entry(request, title):
         "title" : title,
         "titles" : [title],
         "entry" : entry_content,
+        "random" : random_page
     })
 
 
@@ -89,7 +94,11 @@ def search(request, input):
 
 def new(request):
 
+    # Get entries
     entries = util.list_entries()
+
+    # Pick a random page
+    random_page = [random.choice(entries)]
 
     if request.method == "POST":
 
@@ -109,6 +118,7 @@ def new(request):
                 return render(request, "encyclopedia/new.html", {
                     "title_form": form,
                     "placeholder" : "Page already exists",
+                    "random" : random_page,
                 })
             # If page does not already exist
             else:
@@ -125,10 +135,17 @@ def new(request):
     return render(request, "encyclopedia/new.html", {
         "title_form": NewPageForm(),
         "placeholder": "Page context",
+        "random" : random_page,
     })
 
 
 def edit(request, title):
+
+    # Get entries
+    entries = util.list_entries()
+
+    # Pick a random page
+    random_page = [random.choice(entries)]
 
     if request.method == "POST":
 
@@ -155,4 +172,5 @@ def edit(request, title):
     return render(request, "encyclopedia/edit.html", {
         "title" : title,
         "content" : content,
+        "random" : random_page,
     })
