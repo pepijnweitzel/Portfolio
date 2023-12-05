224d90172ddb79e6088e38e7500ffc43ca612b14
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 18:11:44 +0000

    Tue, Dec 5, 2023, 7:11 PM +01:00

--- CS50w/commerce/auctions/views.py
+++ CS50w/commerce/auctions/views.py
@@ -105,3 +105,7 @@ def create(request):
     return render(request, "auctions/create.html", {
         "form" : NewListingForm(),
     })
+
+
+def listing(request):
+    return render(request, "auctions/listing.html")
