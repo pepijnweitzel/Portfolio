e0f9db5e1c637f079ffadd140133c3d6df2ce379
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 14:59:34 +0000

    Tue, Dec 5, 2023, 3:59 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -9,7 +9,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
-    url = models.URLField(default=)
+    url = models.URLField(default="https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg")
     category = models.CharField(max_length=64)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
