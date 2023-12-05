cef5da69512c79637fa0969ebadf5c79fb53c581
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 14:36:35 +0000

    Tue, Dec 5, 2023, 3:36 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -1,17 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-    pass
-
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
     url = models.URLField()
     category = models.CharField(max_length=64)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
     def __str__(self):
         return f"{self.title} selling for ${self.starting_bid}."
+
+
+class User(AbstractUser):
+    listings = models.ManyToManyField(Listing, blank=True, related_name="users")
