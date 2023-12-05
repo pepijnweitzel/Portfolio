b462f6a478626365340cba5ddff34d8053dfd909
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 15:39:29 +0000

    Tue, Dec 5, 2023, 4:39 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -9,7 +9,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
-    url = models.CharField(max_length=1024, default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png")
+    url = models.CharField(max_length=1024)
     category = models.CharField(max_length=64)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
