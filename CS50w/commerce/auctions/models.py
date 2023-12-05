b5c02ee9e89c4beed3b63f57726e4a30389097e4
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 15:15:56 +0000

    Tue, Dec 5, 2023, 4:15 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -9,9 +9,9 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
-    url = models.URLField(default="https://images.app.goo.gl/B3o7pWxL7rVi7wHs9")
+    url = models.CharField(max_length=1024)
     category = models.CharField(max_length=64)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
     def __str__(self):
         return f"{self.title} selling for ${self.starting_bid}."
