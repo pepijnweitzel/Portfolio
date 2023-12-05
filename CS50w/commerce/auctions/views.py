27463955bf420b2079d7858ced1291c6c2e96f64
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 14:00:53 +0000

    Tue, Dec 5, 2023, 3:00 PM +01:00

--- CS50w/commerce/auctions/views.py
+++ CS50w/commerce/auctions/views.py
@@ -82,6 +82,8 @@ def create(request):
         # Take in the data the user submitted and save it as form
         form = NewListingForm(request.POST)
 
+        print("coming")
+
         # Check if form data is valid (server-side)
         if form.is_valid():
 
