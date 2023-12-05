b6f130b267689d77564710115409f6dd45bacd0f
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 14:21:52 +0000

    Tue, Dec 5, 2023, 3:21 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -13,6 +13,5 @@ class Listing(models.Model):
     category = models.CharField(max_length=64)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
-
     def __str__(self):
         return f"{self.title} selling for ${self.starting_bid}."
