7afa22e5c59ac3a7f03c04a15b09b98df623f59a
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-05 15:25:05 +0000

    Tue, Dec 5, 2023, 4:25 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -9,7 +9,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
-    url = models.CharField(max_length=1024, default="https://www.google.com/url?sa=i&url=http%3A%2F%2Fbeepeers.com%2Fassets%2Fimages%2Fcommerces%2F&psig=AOvVaw0CXks0Pp84eKZkY5nVy9Q8&ust=1701875483635000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCJj7vK_K-IIDFQAAAAAdAAAAABAE")
+    url = models.CharField(max_length=1024, default="https://cdn.vectorstock.com/i/1000x1000/79/45/product-image-default-thumbnail-icon-graphic-web-vector-49027945.webp")
     category = models.CharField(max_length=64)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
