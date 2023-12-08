4620995e8744f98828633a0c0aaa15aef69eee3d
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-08 12:04:04 +0000

    Fri, Dec 8, 2023, 1:04 PM +01:00

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -6,11 +6,24 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
+
+    FUN = 'Fun'
+    INSIDE = 'Inside'
+    OUTSIDE = 'Outside'
+    COMPUTER = 'Computer'
+
+    CATEGORY_CHOICES = [
+        (FUN, 'Fun'),
+        (INSIDE, 'Inside'),
+        (OUTSIDE, 'Outside'),
+        (COMPUTER, 'Computer'),
+    ]
+
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.IntegerField()
     url = models.CharField(max_length=1024)
-    category = models.CharField(max_length=64)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     highest_bidder = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist")
