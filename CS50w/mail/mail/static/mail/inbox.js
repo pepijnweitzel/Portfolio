50e75f90dfb3a55d0daa8a7aaae9d0cd94519e8e
pepijnweitzel <138504534+pepijnweitzel@users.noreply.github.com>
2023-12-13 18:18:31 +0000

    Wed, Dec 13, 2023, 7:18 PM +01:00

--- CS50w/mail/mail/static/mail/inbox.js
+++ CS50w/mail/mail/static/mail/inbox.js
@@ -129,7 +129,16 @@ function load_email(email) {
   document.querySelector('#email-view').innerHTML += `<h3>Timestamp: ${email['timestamp']}</h3>`;
 
   // Create archrive button or unarchrive if archrived
+  const newButton = document.createElement('button');
+  if (email['archive'] === false) {
+    newButton.textContent = 'archive';
+  } else {
+    newButton.textContent = 'unarchive';
+  }
   
+
+  document.querySelector('#email-view').appendChild(newButton);
+
 
   // Show body of email
   document.querySelector('#email-view').innerHTML += `<hr><p>${email['body']}</p>`;
